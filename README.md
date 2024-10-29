c829be90cb1baf1afebfdc294a98a1bf6e0e88f1
omria09 <omria09@gmail.com>
2024-10-29 22:44:27 +0200

    part3 complete

---
+++ README.md
@@ -0,0 +1,1 @@
+https://part3-crimson-surf-8273.fly.dev/
--- index.js
+++ index.js
@@ -1,77 +1,118 @@
+require('dotenv').config()
+const Person = require('./models/person')
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const PORT = process.env.PORT
 
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-]
+morgan.token('data', function (req) { return JSON.stringify(req.body) })
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.data(req,res)
+    ].join(' ')
+  })
+)
 
 app.get('/', (request,response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request,response) => {
-    response.json(notes)
+app.get('/info', (request,response) => {
+  const now = new Date(Date.now())
+  Person
+    .find({})
+    .then(person => {response.send(`<p>Phonebook has info for ${person.length} people</p>
+        <p>${now}</p>`)})
 })
 
-app.get('/api/notes/:id', (request, response) =>{
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    note ? response.json(note) : response.status(404).end()
+app.get('/api/persons', (request,response) => {
+  Person
+    .find({})
+    .then(persons => {response.json(persons)})
 })
 
-app.delete('/api/notes/:id', (request, response) =>{
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person){
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person
+    .findByIdAndDelete(request.params.id)
+    .then(deletedPerson => {
+      console.log(`Deleted Person ${deletedPerson} from the database`)
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n=> Number(n.id)))
-    : 0
-    return String(maxId + 1)
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new:true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person ({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-    
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-    
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId()
-    }
+//handler of requests with unknown endpoint
+app.use(unknownEndpoint)
 
-    notes = notes.concat(note)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-    response.json(note)
-})
+  if(error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id' })
+  }else if(error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+//handler of requests with result to errors
+app.use(errorHandler)
 
-const PORT = 3001
-app.listen(PORT, () =>{
-    console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
